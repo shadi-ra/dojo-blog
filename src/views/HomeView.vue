cf24b4aa39dbdfaa968663de9d9b79ccceedf9d2
shadi-ra <shadi_rahbar74@yahoo.com>
2023-08-30 14:32:00 +0330

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+  home 
+<p ref="p"> My Name is {{name}} and my age is{{ age }} </p>
+<button @click="handleClick">click me</button>
+  </div>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default {
+  name: 'HomeView',
+   setup(){
+    const p = ref(null)
+    console.log('setup')
+    let name ='mario'
+    let age = 30
+   
+      const handleClick = () =>{
+        console.log('you clicked me')
+        console.log(p.value.innerHTML)
+        p.value.classList.add('test')
+        p.value.textContent = 'hello,ninjas' 
+      }
+
+    return {name,age,handleClick,p}
+   }
+}
+</script>  
+
