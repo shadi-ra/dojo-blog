c2cd34c0244eafd8e4ebd0d73fc57c1e5f78b6c0
shadi-ra <shadi_rahbar74@yahoo.com>
2023-09-10 14:10:25 +0330

    ...

--- src/components/SinglePost.vue
+++ src/components/SinglePost.vue
@@ -1,12 +1,19 @@
 <template>
    <div class="post">
+     <router-link :to="{ name: 'Details', params:{id: post.id}}">
+        <h3>{{ post.title }}</h3>
+     </router-link>
        <h3>{{ post.title }}</h3>
        <p>{{ snippet }}</p>
+       <span v-for="tag in post.tags" :key="tag">
+              #{{ tag }}
+       </span>
    </div>
 </template>
 
 <script>
   import {computed} from 'vue'
+
 
 export default{
     props:['post'],
